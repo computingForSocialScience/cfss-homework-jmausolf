a4f9f853b47acce200df768e1628c898f7274234
Joshua G. Mausolf <Joshua@joshuas-imac.attlocal.net>
2015-02-03 20:34:59 -0600

    Added code from zip.py to parse.py. Removed zip.py. Added bar.jpg calculated from running parse.py.

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -1,11 +1,14 @@
+# Import Packages
 import csv
 import sys
 import pandas
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from collections import Counter
 
 
+# Read CSV File
 def readCSV(filename):
     '''Reads the CSV file `filename` and returns a list
     with as many items as the CSV has rows. Each list item 
@@ -18,35 +21,19 @@ def readCSV(filename):
     return(lines)
 
 
-### enter your code below
 
 
+
+# *************** Latitude and Longitude Function ******************* #
+
+
+# Data for Functions from CSV
 data = readCSV('permits_hydepark.csv')
 
-data_zip = pandas.read_csv("permits_hydepark.csv", header=None, usecols=[28,35,42, 49, 56, 63, 70, 77, 84])
-
-
-plt.figure();
-data_zip.plot(kind='bar'); 
-plt.show()
-
-"""def zip_list():
-	zip_list = []
-	for i in data_zip:
-		if i != "NaN":
-			z = str(i)
-			zip_list.append[z]
-		return zip_list
-
-print zip_list(), "zip_list" """
-
-
-print "Panda columns: ***"
-print data_zip
-print len(data_zip)
-print type(data_zip)
-
+# INITIAL FUNCTIONS
+# Lat-Long
 def latlongfunt():
+	data = readCSV('permits_hydepark.csv')
 	latlong = []
 	for i in range(0, len(data)):
 		z = str(data[i])
@@ -54,6 +41,7 @@ def latlongfunt():
 		latlong.append(new)
 	return latlong
 
+# Latitude
 def latitude():
 	lat = []
 	for i in range(0, len(data)):
@@ -63,8 +51,8 @@ def latitude():
 		lat.append(lat_data)
 	return lat
 
-
-def longitiude():
+# Longitude
+def longitude():
 	longe= []
 	for i in range(0, len(data)):
 		data_string = str(data[i])
@@ -74,19 +62,21 @@ def longitiude():
 	return longe
 
 
-#print data[1]
 
-
+# Average Latitude-Longitude
 def get_avg_latlng():
-   '''Shows the averge latitude and longitiude of
-   construction permits in Hyde Park'''
+   """Shows the averge latitude and longitude of
+   construction permits in Hyde Park."""
+
+   data = readCSV('permits_hydepark.csv')
+
    lat = latitude()
-   longe = longitiude()
+   longe = longitude()
    for var in data:
       avg_lat = sum(lat)/(len(lat))
       avg_longe = sum(longe)/(len(longe))
    print "*"*40
-   print "Average LONGITUDE and LATITUDE: "
+   print "Average LATITUDE and LONGITUDE: "
    print (avg_lat, avg_longe)
    print "*"*40
 
@@ -94,17 +84,81 @@ get_avg_latlng()
 
 
 
-"""def zip_code():
-	zip_code1 = []
-	for i in range(0, len(data)):
-		data_string = str(data[i])
-		zip_str_data = data_string[150: 250]
-		data = data
-		#zip_data = float(zip_str_data)
-		zip_code1.append(zip_str_data)
-	return zip_code1"""
 
-#print zip_code()
 
-#zip_code_barchart()
+# ***************** Zip Code Function ******************* #
+
+
+# Panda's Data Frame from CSV
+df = pandas.read_csv("permits_hydepark.csv", header=None)
+
+#Individual Data Columns
+df28 = df[28]
+df35 = df[35]
+df42 = df[42]
+df49 = df[49]
+df56 = df[56]
+df63 = df[63]
+df70 = df[70]
+df77 = df[77]
+df84 = df[84]
+
+#Appending the Data Columns as a Panda Series
+#For whatever reason, this would only work one at a time. 
+df_28_35 = df28.append(df35)
+df_28_42 = df_28_35.append(df42)
+df_28_49 = df_28_42.append(df49)
+df_28_56 = df_28_49.append(df56)
+df_28_63 = df_28_56.append(df63)
+df_28_70 = df_28_63.append(df70)
+df_28_77 = df_28_70.append(df77)
+df_28_84 = df_28_77.append(df84)
+
+#Rename combined series
+df_combined = df_28_84
+
+#Removing NaN values
+df_new = pandas.Series.dropna(df_combined)
+
+#Convert Panda Series to Python List
+df_list = df_new.tolist()
+
+# Clean up Zip Codes and Export as a Integer List
+def df_clean():
+	"""Takes the combined Pandas series of zip codes,
+	cleans them, and makes an integer list."""
+	df_clean = []
+	for i in range(0, len(df_list)):
+		new = str(df_list[i])
+		clean = new[0:5]
+		clean_int = int(clean)
+		df_clean.append(clean_int)
+	return df_clean
+
+
+# Define Zip Code Lists Calling the df_clean function
+zip_codes = df_clean()
+
+
+# Make and Save JPG of Zip Code Bar Chart
+def zip_code_barchart():
+	"""Counts the cleaned list of zip codes and makes a 
+	custom bar chart, saving the result as a JPG."""
+
+	# Create counts of zip codes. 
+	zipcode_counts = Counter(zip_codes)
+
+	plt.bar(range(len(zipcode_counts)), zipcode_counts.values(), align='center', color='g')
+	plt.xticks(range(len(zipcode_counts)), zipcode_counts.keys())
+	plt.xticks(rotation=70)
+	plt.yticks(range(0, 6))
+	plt.title('Contractor Zipcode Bar Chart')
+	plt.xlabel('Contractor Zipcodes')
+	plt.ylabel('Frequency')
+	plt.grid(True)
+	plt.draw()
+	plt.savefig('bar.jpg')
+
+
+zip_code_barchart()
 
