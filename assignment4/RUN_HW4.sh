745d9609a3a3434636844aba5ce470f67108c0cd
Joshua G. Mausolf <Joshua@joshuas-imac.attlocal.net>
2015-02-05 11:06:28 -0600

    Updating RUN_HW4 to reflect changes in parse.py among other improvements.

--- assignment4/RUN_HW4.sh
+++ assignment4/RUN_HW4.sh
@@ -1,7 +1,17 @@
+### Designed to Run in MacOSX Terminal. "Open" commands do not run in Linnux.
+echo "This script was designed to run in MAC OSX Terminal."
+echo "It will not have full functionality in Linnux."
+echo "Created by Joshua G. Mausolf"
 
-python parse.py
+sleep 1.5
 
-python parse.py > Avg_lat_long.txt
+#Run LatLong and Print Results to Terminal and text document
+python parse.py "latlong"
+
+python parse.py "latlong" > Avg_lat_long.txt
 open Avg_lat_long.txt
 
-open bar.jpg
+sleep 1
+
+#Run and Open Bar Chart
+python parse.py "bar"
