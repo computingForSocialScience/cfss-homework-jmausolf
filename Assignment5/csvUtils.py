2043f3fc50bbbfb9e9966940533374407cff9add
Joshua G. Mausolf <Joshua@joshuas-imac.attlocal.net>
2015-02-08 18:17:05 -0600

    Updating csvUtils.py. Not done.

--- Assignment5/csvUtils.py
+++ Assignment5/csvUtils.py
@@ -7,8 +7,22 @@ def writeArtistsTable(artist_info_list):
     The csv file should have a header line that looks like this:
     ARTIST_ID,ARTIST_NAME,ARTIST_FOLLOWERS,ARTIST_POPULARITY
     """
-    pass 
+
+    header = u'ARTIST_ID,ARTIST_NAME,ARTIST_FOLLOWERS,ARTIST_POPULARITY'
+    
+
+    f = open('artists.csv', 'w')
+    f.write(header)
+    f.write(u'\n')
+    f.write(artist_info_list)
+    f.close()
       
+
+test_artist_list = u"6vWDO969PvNqNYHIOW5v0m, 'Beyonce', 2539238, 95"
+
+
+writeArtistsTable(test_artist_list)
+
 def writeAlbumsTable(album_info_list):
     """
     Given list of dictionaries, each as returned
@@ -18,4 +32,17 @@ def writeAlbumsTable(album_info_list):
     The csv file should have a header line that looks like this:
     ARTIST_ID,ALBUM_ID,ALBUM_NAME,ALBUM_YEAR,ALBUM_POPULARITY
     """
-    pass
+
+    header = u'ARTIST_ID, ALBUM_ID, ALBUM_NAME, ALBUM_YEAR, ALBUM_POPULARITY'
+    
+
+    f = open('albums.csv', 'w')
+    f.write(header)
+    f.write(u'\n')
+    f.write(album_info_list)
+    f.close()
+
+
+test_album_list = u"6vWDO969PvNqNYHIOW5v0m, 6oxVabMIqCMJRYN1GqR3Vf, 'Dangerously In Love', 2003, 74"
+
+writeAlbumsTable(test_album_list)
