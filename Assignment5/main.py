15d13643c767d69d74ff2f8754507ef1a3b3c6f3
Joshua G. Mausolf <Joshua@joshuas-imac.attlocal.net>
2015-02-08 18:17:37 -0600

    Trying to figure out main.py. Not complete.

--- Assignment5/main.py
+++ Assignment5/main.py
@@ -4,9 +4,49 @@ from fetchAlbums import fetchAlbumIds, fetchAlbumInfo
 from csvUtils import writeArtistsTable, writeAlbumsTable
 from barChart import plotBarChart
 
+def ARTIST(artist_name):
+    fetchArtistId(artist_name)
+    Artist_ID = str(fetchArtistId(artist_name))
+    
+    fetchArtistInfo(Artist_ID)
+    #fetchAlbumIds(Artist_ID)
+
+    Album_ID = fetchAlbumIds(Artist_ID)
+    
+    for ID in Album_ID:
+    	fetchAlbumInfo(ID)
+
+
+## Example syntax to run from terminal shell:
+## python main.py "Zac Brown Band", "Usher", "Blink 182"
+
 if __name__ == '__main__':
     artist_names = sys.argv[1:]
-    print "input artists are ", artist_names
+    print "Input artists are : ", artist_names
     # YOUR CODE HERE
-    
+    for i in artist_names:
+    	ARTIST(i)
 
+
+
+
+
+
+
+
+"""if __name__ == '__main__':
+    artist_names = sys.argv[1:]
+    print "Input artists are : ", artist_names
+    # YOUR CODE HERE
+    #artist_names = ["Justin Timberlake", "Usher", "Zac Brown Band"]
+    for i in artist_names:
+    	fetchArtistId(i)
+    	Artist_ID = str(fetchArtistId(i))
+    
+    	fetchArtistInfo(Artist_ID)
+    	#fetchAlbumIds(Artist_ID)
+
+       	Album_ID = fetchAlbumIds(Artist_ID)
+    
+    	for ID in Album_ID:
+    		fetchAlbumInfo(ID)"""
